eabf0fd6fdb2fb14ce0f321119b98c8f83c19b0c
Francesco Voto <francesco.voto.8@gmail.com>
2017-11-17 16:51:18 +0100

    feat: set default state on Book selection if shelf is null or undefined

--- src/Book.js
+++ src/Book.js
@@ -5,15 +5,14 @@ import './App.css'
 const Book = (props) => {
 
   const { updateBook, book: { imageLinks: { thumbnail }, title, authors, shelf }} = props;
-
-  console.log(props);
+  
   return(
     <div className="book">
       <div className="book-top">
         <div className="book-cover"
              style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})` }} />
         <div className="book-shelf-changer">
-          <select value={shelf} onChange={(event) => updateBook(event.target.value)}>
+          <select value={shelf || 'none'} onChange={(event) => updateBook(event.target.value)}>
             <option value="none" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
